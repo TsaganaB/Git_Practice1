24e6e4a2b5da44f83a6af921258ce9acaaf6f716
Galsan <Angira8080@gmail.com>
2022-04-06 13:04:34 -0400

    added more work

--- src/practice1/Water.java
+++ src/practice1/Water.java
@@ -4,5 +4,6 @@ public class Water {
     public static void main(String[] args) {
         System.out.println("Don't forget to drink more water");
         System.out.println("Created Git Repository");
+        System.out.println("Made few commit");
     }
 }
